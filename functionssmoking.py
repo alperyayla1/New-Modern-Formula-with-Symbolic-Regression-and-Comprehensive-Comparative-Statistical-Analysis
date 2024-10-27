d4fdcc1178da9f1aee1ebeb2494acb69eed8e481
Alper <103644045+alperyayla1@users.noreply.github.com>
2024-10-27 20:04:52 +0300

    Delete functionssmoking.py

--- functionssmoking.py
+++
@@ -1,9 +0,0 @@
-def drop_outliers(data, column_name):
-    q1 = data[column_name].quantile(0.25)
-    q3 = data[column_name].quantile(0.75)
-    IQR = q3 - q1
-    lower_bound = q3 - 1.5 * IQR
-    upper_bound = q3 + 1.5 * IQR
-    data = data[(data[column_name] >= lower_bound) & (data[column_name] <= upper_bound)]
-    print(lower_bound, upper_bound)
-    return data
